64a2d72bc5d5bfffbefc3daf4451e77450f7d655
Nourhan Ahmed El-shiekh <nourhanelshiekh71@gmail.com>
2026-09-13 23:12:29 +0300

    Add a simple HTTP server in app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Hello from Python Automated CI/CD Pipeline!")
+
+if __name__ == '__main__':
+    server = HTTPServer(('0.0.0.0', 5000), SimpleHandler)
+    print("Server running on port 5000...")
+    server.serve_forever()
