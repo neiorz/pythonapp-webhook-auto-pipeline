17ab430ec2bc4e1a51ae29df09c48dcb60380330
Nourhan Ahmed El-shiekh <nourhanelshiekh71@gmail.com>
2026-09-14 00:30:03 +0300

    Change server startup message to 'NOW Server running' new new new

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ class SimpleHandler(BaseHTTPRequestHandler):
 
 if __name__ == '__main__':
     server = HTTPServer(('0.0.0.0', 5000), SimpleHandler)
-    print("Server running on port 5000...")
+    print("NOW Server running on port 5000...")
     server.serve_forever()
